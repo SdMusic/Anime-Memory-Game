05325b8201cf5683771edc4cd62ce082f3c1a7e8
SdMusic <peter_freemantle@hotmail.com>
2021-02-02 21:18:16 +0000

    jsworking css styling to be done

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-//the cards
+
+//The Cards
+
 const cardArray = [
     {
         name: 'ace',
@@ -49,68 +51,128 @@ const cardArray = [
         name: 'franky',
         img: 'images/franky.png'
     }
-]
+];
 
-  cardArray.sort(() => 0.5 - Math.random())
+//The Card Shuffle
 
-  const grid = document.querySelector('.grid')
-  const resultDisplay = document.querySelector('#result')
-  let cardsChosen = []
-  let cardsChosenId = []
-  let cardsWon = []
+  cardArray.sort(() => 0.5 - Math.random());
 
-  //create your board
+  const grid = document.querySelector('.grid');
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+
+//Creating the Board
+
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-      const card = document.createElement('img')
-      card.setAttribute('src', 'images/blank.jpg')
-      card.setAttribute('data-id', i)
-      card.addEventListener('click', flipCard)
-      grid.appendChild(card)
+      const card = document.createElement('img');
+      card.classList.add('hvr-float-shadow');
+      card.setAttribute('src', 'images/blank.jpg');
+      card.setAttribute('data-id', i);
+      card.addEventListener('click', flipCard);
+      grid.appendChild(card);
     }
   }
 
-  //check for matches
+//Card Match Check
+
   function checkForMatch() {
-    const cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
     
     if(optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', 'images/blank.jpg')
-      cards[optionTwoId].setAttribute('src', 'images/blank.jpg')
-      alert('Thats the same one!')
+      cards[optionOneId].setAttribute('src', 'images/blank.jpg');
+      cards[optionTwoId].setAttribute('src', 'images/blank.jpg');
+      alert('Thats the same one!');
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
-      alert('Im going to be pirate king!')
-      cards[optionOneId].setAttribute('src', 'images/white.jpg')
-      cards[optionTwoId].setAttribute('src', 'images/white.jpg')
-      cards[optionOneId].removeEventListener('click', flipCard)
-      cards[optionTwoId].removeEventListener('click', flipCard)
-      cardsWon.push(cardsChosen)
+      alert('Im going to be pirate king!');
+      cards[optionOneId].classList.remove('hvr-float-shadow');
+      cards[optionTwoId].classList.remove('hvr-float-shadow');
+      cards[optionOneId].removeEventListener('click', flipCard);
+      cards[optionTwoId].removeEventListener('click', flipCard);
+      cardsWon.push(cardsChosen);
     } else {
-      cards[optionOneId].setAttribute('src', 'images/blank.jpg')
-      cards[optionTwoId].setAttribute('src', 'images/blank.jpg')
-      alert('That isnt enough to stop me!')
+      cards[optionOneId].setAttribute('src', 'images/blank.jpg');
+      cards[optionTwoId].setAttribute('src', 'images/blank.jpg');
+      alert('That isnt enough to stop me!');
     }
-    cardsChosen = []
-    cardsChosenId = []
-    resultDisplay.textContent = cardsWon.length
+    cardsChosen = [];
+    cardsChosenId = [];
     if  (cardsWon.length === cardArray.length/2) {
-      resultDisplay.textContent = 'Yattaaaa!!! You are the Pirate King!!'
+      resultDisplay.textContent = 'Yattaaaa!!! You are the Pirate King!!';
+      stopMusic();
+      stopTimer();
+      win.play();
+      finalScore();
     }
   }
 
-  //flip your card
+// Card Flip
+
   function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    flipSound.play();
+    moveCounter();
     if (cardsChosen.length ===2) {
-      setTimeout(checkForMatch, 500)
+      setTimeout(checkForMatch, 500);
     }
   }
 
-  createBoard()
-})
+
+  createBoard();
+});
+
+//The Sound Board
+
+var bgmusic = new Audio('assets/audio/bg-music.mp3');
+var win = new Audio('assets/audio/win.wav');
+var flipSound = new Audio('assets/audio/woosh.wav');
+flipSound.volume = 0.1;
+
+
+function stopMusic(){
+    bgmusic.pause();
+    bgmusic.currentTime = 0;
+}
+
+//The Timer
+
+var timer = setInterval(countTimer, 1000);
+var totalSeconds = 0;
+function countTimer() {
+           ++totalSeconds;
+           var minute = Math.floor(totalSeconds /60);
+           var seconds = totalSeconds - (minute*60);
+           if(minute < 10)
+             minute = "0"+minute;
+           if(seconds < 10)
+             seconds = "0"+seconds;
+           document.getElementById("timer").innerHTML =  minute + ":" + seconds;
+           
+        }
+
+
+function stopTimer() {
+      clearInterval(timer);
+    }
+    
+//Move Counter
+
+let moves = 0;
+const counter = document.querySelector(".moves");
+function moveCounter() {
+  moves++;
+  counter.innerHTML = moves;
+}
+
+//Final Score
+
+function finalScore(){
+   document.getElementById("score").innerHTML = moves/totalSeconds*1000;
+}
